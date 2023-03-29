29012494bb409f55433b411d33a206090b81ee20
AOhapkin <alexeyyago@gmail.com>
2023-03-29 15:27:27 +0300

    some utils

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+function getRandomPositiveInt(a, b) {
+    const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+    const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+
+    const randomResult = Math.random() * (max - min + 1) + min;
+
+    return Math.floor(randomResult);
+}
+
+function checkStringLength(string, maxLength) {
+    return string.length <= maxLength;
+}
