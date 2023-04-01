33c14ef3b446640f9fe566382295466a1c70d2e8
AOhapkin <alexeyyago@gmail.com>
2023-04-01 09:18:48 +0300

    show comments list

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,11 +1,15 @@
 const bigPicture = document.querySelector('.big-picture');
 const image = bigPicture.querySelector('img');
+const imageCaption = bigPicture.querySelector('.social__caption');
 const likesCount = bigPicture.querySelector('.likes-count');
 const commentsCounterBlock = bigPicture.querySelector('.social__comment-count');
 const commentsTotalCount = bigPicture.querySelector('.comments-count');
 const commentsList = bigPicture.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
 let comments = [];
+const MIN_COMMENTS_NUMBER = 5;
+let maxCommentsOnDisplay = MIN_COMMENTS_NUMBER;
 
 function clearComments() {
     commentsList.innerHTML = '';
@@ -15,19 +19,40 @@ function setCommentCounter() {
     commentsCounterBlock.innerHTML = `${commentsList.children.length} из <span class="comments-count">${comments.length}</span> комментариев`;
 }
 
+function createComment(commentData) {
+    let comment = commentTemplate.cloneNode(true);
+    let image = comment.querySelector('.social__picture');
+    let text = comment.querySelector('.social__text');
+
+    image.src = commentData.avatar;
+    image.alt = commentData.name;
+    text.textContent = commentData.message;
+
+    return comment;
+}
+
+function showComments() {
+    const fragment = document.createDocumentFragment();
+    comments.slice(0, maxCommentsOnDisplay).forEach((comment) => {
+        fragment.append(createComment(comment));
+    });
+    commentsList.append(fragment);
+}
+
 function renderBigPicture(data) {
-    console.log(data);
+    // console.log(data);
 
     image.src = data.url
     image.alt = data.description;
+    imageCaption.textContent = data.description;
     likesCount.textContent = data.likes;
-    commentsTotalCount.textContent = data.comments.length;
+    commentsTotalCount.textContent = data.comments.length + '';
     comments = data.comments;
     clearComments();
     if (comments.length > 0) {
-        // showComments();
+        showComments();
     }
-    setCommentCounter();
+    // setCommentCounter();
 }
 
 function showBigPicture(pictureData) {
